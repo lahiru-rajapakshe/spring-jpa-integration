ee40c10c26a05b1ce07d240ebc13d43b74f76e1f
LahiruRJ <sanlahiru720@gmail.com>
2022-06-20 16:53:45 +0530

    Add LocalContainerEntityManagerFactoryBean

--- src/main/java/lk/lahiru/jpaspringintegration/tasks/config/JpaCofig.java
+++ src/main/java/lk/lahiru/jpaspringintegration/tasks/config/JpaCofig.java
@@ -2,4 +2,18 @@ package lk.lahiru.jpaspringintegration.tasks.config;
 
 public class JpaCofig {
     private final Environment env;
+
+
+    public JPAConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpaVendorAdapter){
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setDataSource(ds);
+        lcemfb.setJpaVendorAdapter(jpaVendorAdapter);
+        lcemfb.setPackagesToScan("lk.ijse.dep8.tasks.entity");
+        return lcemfb;
+    }
 }
